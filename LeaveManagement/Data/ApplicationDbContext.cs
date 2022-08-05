537097229c01f445df10350ed1b46842a9797829
Emre-Gelen <emregeln@gmail.com>
2022-08-05 21:10:53 +0300

    Updated _LoginPartial

--- LeaveManagement/Data/ApplicationDbContext.cs
+++ LeaveManagement/Data/ApplicationDbContext.cs
@@ -1,10 +1,9 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace LeaveManagement.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    public class ApplicationDbContext : IdentityDbContext<Employee>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
